41f9a47894998300c2427ab32c28394e7d7daadc
Rafael Carvalho <rapha.pse@outlook.com>
2018-05-27 20:34:18 -0300

    Add App

--- uBER/app/src/main/java/com/br/uber/MainActivity.java
+++ uBER/app/src/main/java/com/br/uber/MainActivity.java
@@ -3,11 +3,19 @@ package com.br.uber;
 import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private Button btnsingUp, btnRegister;
+
+    private FirebaseAuth auth;
 
 
     @Override
@@ -23,6 +31,9 @@ public class MainActivity extends AppCompatActivity {
         CalligraphyConfig.initDefault(new CalligraphyConfig.Builder()
                                         .setDefaultFontPath("fonts/arkhip_font.ttf")
                                         .setFontAttrId(R.attr.fontPath).build()    );
+
+        btnsingUp = (Button) findViewById(R.id.btn_SingUp);
+        btnRegister = (Button) findViewById(R.id.btn_Register);
     }
 
     //parei no tempo 20:28 video 1
